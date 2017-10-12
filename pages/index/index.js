3bc1a362262d3e0d2a002e9d191daf1807273c9b
fuhao <465476662@qq.com>
2017-10-12 16:26:49 +0800

    首页增加动画效果

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,64 @@ const app = getApp();
 
 Page({
     data: {
-        userInfo: {}
+        imgSrc: '../../img/',
+        userInfo: {},
+        animationOne: {},
+        animationTwo: {},
+        animationThree: {},
+        animationFour: {},
+    },
+    animationOne() {
+        const animation = wx.createAnimation({
+            duration: 3000,
+            timingFunction: 'ease',
+            delay: 1000,
+        })
+
+        animation.translateX(160).rotate(360).step();
+
+        this.setData({
+            animationOne: animation.export(),
+        });
+    },
+    animationTwo() {
+        const animation = wx.createAnimation({
+            duration: 3000,
+            timingFunction: 'ease',
+            delay: 1000,
+        })
+
+        animation.translateX(160).rotate(360).step();
+
+        this.setData({
+            animationTwo: animation.export(),
+        });
+    },
+    animationThree() {
+        const animation = wx.createAnimation({
+            duration: 3000,
+            timingFunction: 'ease',
+            delay: 1000,
+        })
+
+        animation.translateX(-160).rotate(-360).step();
+
+        this.setData({
+            animationThree: animation.export(),
+        });
+    },
+    animationFour() {
+        const animation = wx.createAnimation({
+            duration: 3000,
+            timingFunction: 'ease',
+            delay: 1000,
+        })
+
+        animation.translateX(-160).rotate(-360).step();
+
+        this.setData({
+            animationFour: animation.export(),
+        });
     },
     onLoad(options) {
         app.getUserInfo().then(userInfo => {
@@ -16,7 +73,13 @@ Page({
 
     },
     onShow() {
+        this.animationOne();
+        this.animationTwo();
 
+        setTimeout(() => {
+            this.animationThree();
+            this.animationFour();
+        }, 3500)
     },
     onUnload() {
 
